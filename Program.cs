aeeadf39a3f2068a39bb8eba4058e94779fe6d64
Balanjaneyasharma <balanjaneyasarma@gmail.com>
2026-04-26 23:53:13 +0530

    Remove weather forecast commented code in program.cs

--- Program.cs
+++ Program.cs
@@ -13,9 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Register application services
-var connectionString = builder.Configuration.GetConnectionString("DBConnection")
-    ?? throw new InvalidOperationException("Connection string 'DBConnection' is not configured.");
-builder.Services.AddApplication(connectionString);
+builder.Services.AddApplication(builder.Configuration.GetConnectionString("DBConnection"));
 
 // Add authentication / authorization
 builder.Services.AddAuthentication();
@@ -40,30 +38,3 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
-
-/**
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-**/
