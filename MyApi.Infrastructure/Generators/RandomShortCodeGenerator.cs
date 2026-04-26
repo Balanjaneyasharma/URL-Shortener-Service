322ca783c556c09d8cb8f19f98e9b2a2828800e4
Balanjaneyasharma <balanjaneyasarma@gmail.com>
2026-04-26 23:32:05 +0530

    Add short code generator

---
+++ MyApi.Infrastructure/Generators/RandomShortCodeGenerator.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography;
+
+using MyApi.Domain.Interfaces;
+
+public class RandomShortCodeGenerator: IShortCodeGenerator
+{
+    public string Generate()  
+    {  
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";  
+        var bytes = new byte[8];  
+        RandomNumberGenerator.Fill(bytes);  
+    
+       return new string(bytes.Select(b => chars[b % chars.Length]).ToArray());
+    }
+}
