30aed36d4354925a36604547f280de9563f8abd0
Balanjaneyasharma <balanjaneyasarma@gmail.com>
2026-04-26 23:28:58 +0530

    Add UrlMapping database entity

---
+++ MyApi.Infrastructure/Entities/UrlMappingEntity.cs
@@ -0,0 +1,9 @@
+namespace MyApi.Infrastructure.Entities;
+public class UrlMappingEntity
+{
+    public int Id { get; set; }
+    public string ShortUrl { get; set; } = null!;
+    public string LongUrl { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
